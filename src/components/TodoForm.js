8da43d2210a941734a3555c38c4a62e404ffa273
Barbara Moore <barbaraloismoore@gmail.com>
2020-06-01 19:09:30 -0400

    mvp completed

---
+++ src/components/TodoForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class TodoForm extends React.Component {
+  render() {
+    return (
+      <form onSubmit={this.props.handleSubmit}>
+        <input
+          type="text"
+          name="task"
+          value={this.props.currentInput}
+          onChange={this.props.handleChanges}
+        />
+        <button>Add</button>
+        <button onClick={this.props.clearCompleted}>
+          Clear Completed Tasks
+        </button>
+      </form>
+    );
+  }
+}
